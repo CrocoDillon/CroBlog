598d647eb793b225f2ae6a0a43bd8ad2bb9802c7
Dillon de Voor <dillon@crocodillon.com>
2014-04-29 21:51:56 +0200

    Fix skeleton unit tests.

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -2,6 +2,6 @@
 return array(
     'CroBlog\Module'                    => __DIR__ . '/Module.php',
     'CroBlog\Controller\BlogController' => __DIR__ . '/src/CroBlog/Controller/SkeletonController.php',
-    'CroBlog\Framework\TestCase'        => __DIR__ . '/tests/CroBlog/Framework/TestCase.php',
-    'CroBlog\SampleTest'                => __DIR__ . '/tests/CroBlog/SampleTest.php',
+    'CroBlogTest\Framework\TestCase'    => __DIR__ . '/tests/CroBlog/Framework/TestCase.php',
+    'CroBlogTest\SampleTest'            => __DIR__ . '/tests/CroBlog/SampleTest.php',
 );
--- tests/CroBlog/Framework/TestCase.php
+++ tests/CroBlog/Framework/TestCase.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
-namespace ZendSkeletonModuleTest\Framework;
+namespace CroBlogTest\Framework;
 
 use PHPUnit_Framework_TestCase;
 
